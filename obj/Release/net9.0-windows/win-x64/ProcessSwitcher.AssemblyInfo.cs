974e7a15891b5fb5b614984215c5846e1423a04e
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-02-04 14:43:44 -0300

    Fix

--- obj/Release/net9.0-windows/win-x64/ProcessSwitcher.AssemblyInfo.cs
+++ obj/Release/net9.0-windows/win-x64/ProcessSwitcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcessSwitcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7267f2bd3423c28f1dab93aa08463963036c001e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ad55ba15ef33609e2e837b27e7759f195f903b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcessSwitcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcessSwitcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
