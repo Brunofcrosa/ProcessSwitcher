7fc7e0b441948a12bd5315c1d1a188cd92a37abf
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-02-04 14:31:44 -0300

    Fix bugs

--- obj/Debug/net9.0-windows/ProcessSwitcher.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/ProcessSwitcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcessSwitcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bfb45e456c3172fb376e88c164098a4ce54721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a172c3be05ae7deb3c1c17648d9bafafd122b4d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcessSwitcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcessSwitcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
